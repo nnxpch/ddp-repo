63a517bfaef823dfb604d335e580c1746381d9d4
nnxpch <preecharin.n@gmail.com>
2024-01-23 22:56:41 +0000

    further practice

--- python-wk1/practice.py
+++ python-wk1/practice.py
@@ -1,4 +1,20 @@
-name = "Alice"
+""" name = "Alice"
 age = 25
 message = f"Hello, {name}. You are {age} years old."
-print(message)  # Output: Hello, Alice. You are 25 years old.
+print(message)  # Output: Hello, Alice. You are 25 years old. """
+
+def calsec(h,m,s):
+    h = h * 60 * 60
+    m = m * 60
+    s = s
+    return h + m + s
+
+
+def sumsec():
+    h = int(input("Hour: "))
+    m = int(input("Minute: "))
+    s = int(input("Second: "))
+
+    print("Summary:", calsec(h, m, s))
+
+sumsec()
