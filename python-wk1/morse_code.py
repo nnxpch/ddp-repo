a5cdc409fa03f944fa58f9d1e978f750fe64ca4b
nnxpch <143273881+nnxpch@users.noreply.github.com>
2024-01-26 01:43:02 +0000

    morse code practice

---
+++ python-wk1/morse_code.py
@@ -0,0 +1,69 @@
+"""
+Write a Python function named morse_translator that translates a given string into Morse code.
+
+Each alphabetic character in the string should be translated to its corresponding Morse code equivalent.
+The Morse code for each character should be separated by a space.
+Each word in the string should be separated by a forward slash (/).
+The function should handle both uppercase and lowercase alphabetic characters.
+The function should be case-insensitive, meaning it treats uppercase and lowercase letters the same.
+Non-alphabetic characters (like numbers or symbols) should not be translated.
+
+https://en.wikipedia.org/wiki/Morse_code
+"""
+
+
+def morse_translator(text):
+    # Morse code mapping
+    morse_code_dict = {
+        "A": ".-",
+        "B": "-...",
+        "C": "-.-.",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "V": "...-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+    }
+
+    # Your code goes here
+
+    # convert text input to uppercase
+    text = text.upper()
+
+    morse_code_list = []
+    for alphabet in text.split():
+        morse_code = [] 
+        for key in alphabet:
+            morse_code.append(morse_code_dict[key])
+            #print(morse_code)
+        morse_code_list.append(' '.join(morse_code))
+
+    # Join the Morse code for each word with a forward slash
+    morse_result = '/'.join(morse_code_list)
+
+    return morse_result
+            
+
+
+# Test cases
+print(morse_translator("HELLO WORLD"))  # Expected output: ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
+print(morse_translator("Python"))  # Expected output: ".--. -.-- - .... --- -."
+print(morse_translator("Morse Code"))  # Expected output: "-- --- .-. ... . / -.-. --- -.. ."
